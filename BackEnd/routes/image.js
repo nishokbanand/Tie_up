41cd5b0d55ee8d742414b0a768677052aa48ddad
nishokbanand <nishokbanand@gmail.com>
2022-04-29 08:32:35 +0530

    post added

--- BackEnd/routes/image.js
+++ BackEnd/routes/image.js
@@ -13,6 +13,7 @@ var imageUploader = async function (req, res) {
       image: req.files.file.data.toString("base64"),
     });
     value = a;
+    res.redirect("http://localhost:5500/FrontEnd/home.html");
     return res.json({ status: "ok", data: a });
   } catch (error) {
     console.log(error);
--- FrontEnd/JavaScript/home.js
+++ FrontEnd/JavaScript/home.js
@@ -11,6 +11,18 @@ window.onload = async () => {
   const data = await response.json();
   console.log(data);
   for (var i = 0; i < data.length; i++) {
-    $(".images").append(`<div class="col-md-4"><p>${data[i].title}</p></div>`);
+    $(".title_div").append(
+      `<div class="col-md-4"><p>${data[i].title}</p></div>`
+    );
+    $(".description_div").append(
+      `<div class="post col col-md-12 col-lg-7 col-sm-12 col-xs-12">${data[i].description}</div>`
+    );
+    //convert base64 to image
+    var base64 = data[i].image;
+    var image = document.createElement("img");
+    image.src = "data:image/png;base64," + base64;
+    $(".image_div").append(
+      `<div class="col-md-4"><img src="${image.src}"/></div>`
+    );
   }
 };
